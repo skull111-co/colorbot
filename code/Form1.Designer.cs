bf76a2eb2c7542233832dcc266f4631d80963dbb
KaanGaming <kaangaming.gamejolt@gmail.com>
2019-02-03 21:03:10 +0300

    Add files via upload
    Added code files into the "code" folder.

---
+++ code/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace COLORBOT
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.combo_Color = new System.Windows.Forms.ComboBox();
+			this.username = new System.Windows.Forms.TextBox();
+			this.password = new System.Windows.Forms.TextBox();
+			this.worldid = new System.Windows.Forms.TextBox();
+			this.toolTip_worldid = new System.Windows.Forms.ToolTip(this.components);
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.code = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// combo_Color
+			// 
+			this.combo_Color.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.combo_Color.FormattingEnabled = true;
+			this.combo_Color.Items.AddRange(new object[] {
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Cyan",
+            "Blue",
+            "Purple"});
+			this.combo_Color.Location = new System.Drawing.Point(315, 13);
+			this.combo_Color.Name = "combo_Color";
+			this.combo_Color.Size = new System.Drawing.Size(145, 21);
+			this.combo_Color.TabIndex = 0;
+			// 
+			// username
+			// 
+			this.username.Location = new System.Drawing.Point(13, 13);
+			this.username.Name = "username";
+			this.username.Size = new System.Drawing.Size(203, 20);
+			this.username.TabIndex = 1;
+			// 
+			// password
+			// 
+			this.password.Location = new System.Drawing.Point(13, 39);
+			this.password.Name = "password";
+			this.password.Size = new System.Drawing.Size(203, 20);
+			this.password.TabIndex = 2;
+			// 
+			// worldid
+			// 
+			this.worldid.Location = new System.Drawing.Point(12, 65);
+			this.worldid.Name = "worldid";
+			this.worldid.Size = new System.Drawing.Size(203, 20);
+			this.worldid.TabIndex = 3;
+			// 
+			// toolTip_worldid
+			// 
+			this.toolTip_worldid.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+			this.toolTip_worldid.ToolTipTitle = "About World ID";
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Location = new System.Drawing.Point(12, 92);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(204, 46);
+			this.btnConnect.TabIndex = 4;
+			this.btnConnect.Text = "Connect";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+			// 
+			// code
+			// 
+			this.code.Location = new System.Drawing.Point(347, 118);
+			this.code.Name = "code";
+			this.code.Size = new System.Drawing.Size(112, 20);
+			this.code.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(222, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Email";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(222, 42);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Password";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(222, 68);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(92, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "World ID (room id)";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(309, 121);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(32, 13);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Code";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(472, 150);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.code);
+			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.worldid);
+			this.Controls.Add(this.password);
+			this.Controls.Add(this.username);
+			this.Controls.Add(this.combo_Color);
+			this.Name = "Form1";
+			this.Text = "COLORBOT";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox combo_Color;
+		private System.Windows.Forms.TextBox username;
+		private System.Windows.Forms.TextBox password;
+		private System.Windows.Forms.TextBox worldid;
+		private System.Windows.Forms.ToolTip toolTip_worldid;
+		private System.Windows.Forms.Button btnConnect;
+		private System.Windows.Forms.TextBox code;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ code/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using PlayerIOClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace COLORBOT
+{
+	public partial class Form1 : Form
+	{
+		private Client client;
+		private Connection con;
+		private bool isConnected;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			toolTip_worldid.SetToolTip(worldid, "It's the Room ID! To get this info, you need to join a world and do: /roomid");
+		}
+
+		private void btnConnect_Click(object sender, EventArgs e)
+		{
+			int errors = 0;
+
+			if (string.IsNullOrWhiteSpace(combo_Color.Text))
+			{
+				MessageBox.Show("No. It's not like you will not select the color, and I WILL!", "Error count: " + (errors + 1), MessageBoxButtons.OK, MessageBoxIcon.Error);
+				errors++;
+			}
+			if (string.IsNullOrWhiteSpace(worldid.Text))
+			{
+				MessageBox.Show("Please tell our destination.", "Error count: " + (errors + 1), MessageBoxButtons.OK, MessageBoxIcon.Error);
+				errors++;
+			}
+			if (string.IsNullOrWhiteSpace(username.Text))
+			{
+				MessageBox.Show("Who am I?", "Error count: " + (errors + 1), MessageBoxButtons.OK, MessageBoxIcon.Error);
+				errors++;
+			}
+			if (string.IsNullOrWhiteSpace(password.Text))
+			{
+				MessageBox.Show("Wow. IT'S NOT LIKE I KNOW YOUR PASSWORD.\nI am not a spyware, what did you thought? Go find it yourself.", "Error count: " + (errors + 1), MessageBoxButtons.OK, MessageBoxIcon.Error);
+				errors++;
+			}
+
+			if (errors == 1)
+			{
+				MessageBox.Show("Uh, you forgot one thing. If you did that, then it's all ready!");
+			}
+			else if (errors == 2)
+			{
+				MessageBox.Show("Did you forgot to set your username and password, or... the other thing? No problem for you, it's just for me.");
+			}
+			else if (errors == 3)
+			{
+				MessageBox.Show("Not only the color/world id/username/password! All the other things! Nah, code is not required, only if the owner's here and will give you edit.");
+			}
+			else if (errors == 4)
+			{
+				MessageBox.Show("You are lazy.");
+			}
+
+			if (errors > 0) return;
+			else
+			{
+				if (!isConnected) MessageBox.Show("Good! All things're ready.");
+			}
+
+			if (!isConnected)
+			{
+				try
+				{
+					client = PlayerIO.QuickConnect.SimpleConnect("everybody-edits-su9rn58o40itdbnw69plyw", username.Text, password.Text, null);
+					con = client.Multiplayer.CreateJoinRoom(worldid.Text, "public", true, new Dictionary<string, string>(), new Dictionary<string, string>());
+
+					con.Send("init");
+					con.OnMessage += new MessageReceivedEventHandler(handlemsg);
+					isConnected = true;
+					btnConnect.Text = "Disconnect";
+					combo_Color.Enabled = false;
+				}
+				catch
+				{
+					MessageBox.Show("Error! I hope something didn't went wrong.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+			else
+			{
+				con.Send("say", "[COLORBOT] We are going away. The fun time's over!");
+				con.Disconnect();
+				isConnected = false;
+				btnConnect.Text = "Connect";
+				combo_Color.Enabled = true;
+			}
+		}
+
+		private void handlemsg(object sender, PlayerIOClient.Message m)
+		{
+			if (m.Type == "b")
+			{
+				int blockid = 0;
+				if (combo_Color.Text == "Red") blockid = 70;
+				if (combo_Color.Text == "Orange") blockid = 76;
+				if (combo_Color.Text == "Yellow") blockid = 75;
+				if (combo_Color.Text == "Green") blockid = 74;
+				if (combo_Color.Text == "Cyan") blockid = 73;
+				if (combo_Color.Text == "Blue") blockid = 72;
+				if (combo_Color.Text == "Purple") blockid = 71;
+
+				if (m.GetUInt(3) == 0) return;
+				else con.Send("b", m.GetUInt(0), m.GetUInt(1), m.GetUInt(2), blockid);
+			}
+			else if (m.Type == "init")
+			{
+				con.Send("say", "[COLORBOT] Hi y'all. I am here just because for fun.");
+				Thread.Sleep(500);
+				con.Send("say", "[COLORBOT] Oh and also, I am made by skull111. But still, no commands!");
+				Thread.Sleep(500);
+				con.Send("say", "[COLORBOT] I will try the code if it works. If it does, then I will want from the owner to give edit.");
+				Thread.Sleep(500);
+				con.Send("say", "[COLORBOT] Current color: " + combo_Color.Text + ".");
+				Thread.Sleep(500);
+				con.Send("access", code.Text);
+			}
+		}
+	}
+}
